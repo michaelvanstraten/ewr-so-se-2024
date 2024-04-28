808bdfe7ee018e1d31692c72fc9a284a6b0e9f60
Michael van Straten <michael@vanstraten.de>
2024-04-28 23:49:14 +0200

    Add basic setup files

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      poetry2nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages.default = mkPoetryApplication { projectDir = self; };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = [ pkgs.poetry ];
+        };
+      }
+    );
+}
