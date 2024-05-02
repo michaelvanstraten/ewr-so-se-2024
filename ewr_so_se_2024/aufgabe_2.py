7a9c5d09d57683f8f950085a11625cee726c425d
Michael van Straten <michael@vanstraten.de>
2024-05-02 11:32:44 +0200

    Add ability to set output data type and format file

--- ewr_so_se_2024/aufgabe_2.py
+++ ewr_so_se_2024/aufgabe_2.py
@@ -1,16 +1,24 @@
 from functools import partial
 from itertools import accumulate, repeat
 
-def py_logspace(start: int, stop: int, num: int = 5, basis: int = 10) -> list[int]:
-    if num < 3: raise ValueError
-    offset = repeat((stop - start)/(num - 1), num - 1)
+from typing import Any
+
+
+def py_logspace(
+    start: int, stop: int, num: int = 5, basis: int = 10, dtype: Any = int
+) -> list:
+    if num < 3:
+        raise ValueError
+    offset = repeat((stop - start) / (num - 1), num - 1)
     # Would be cleaner with https://github.com/EntilZha/PyFunctional
-    items = map(int, map(partial(pow, basis), accumulate(offset, initial = start)))
+    items = map(dtype, map(partial(pow, basis), accumulate(offset, initial=start)))
     # Would be more efficiant if they let us return an iterator instead of a list
     return list(items)
 
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
