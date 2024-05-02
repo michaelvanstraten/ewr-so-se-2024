e9ec04cf76c447aa99369ae511f8409d9f8195b9
Michael van Straten <michael@vanstraten.de>
2024-05-02 12:37:57 +0200

    Make figures plotable

--- ewr_so_se_2024/aufgabe_2.py
+++ ewr_so_se_2024/aufgabe_2.py
@@ -3,6 +3,8 @@ from itertools import accumulate, repeat
 
 from typing import Any
 
+import click
+
 
 def py_logspace(
     start: int, stop: int, num: int = 5, basis: int = 10, dtype: Any = int
@@ -16,8 +18,25 @@ def py_logspace(
     return list(items)
 
 
-def main():
-    pass
+@click.command()
+@click.argument("start", type=int)
+@click.argument("stop", type=int)
+@click.option("-s", "--number-of-samples", default=5, type=int)
+@click.option("--basis", default=10, type=float)
+def main(start, stop, number_of_samples, basis):
+    import matplotlib.pyplot as plt
+
+    sample = py_logspace(start, stop, num=number_of_samples, basis=basis)
+
+    fig = plt.figure(
+        f"py_logspace({start}, {stop}, num={number_of_samples}, basis={basis})"
+    )
+    axes = fig.subplots(nrows=1, ncols=2)
+    axes[0].plot(sample)
+    axes[1].plot(sample)
+    axes[1].set_yscale("log")
+
+    plt.show()
 
 
 if __name__ == "__main__":
