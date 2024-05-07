443195553ac9b682e6652907a5b750f7d9e9b22a
Michael van Straten <michael@vanstraten.de>
2024-05-07 21:32:26 +0200

    Format file

--- ewr_so_se_2024/aufgabe_2.py
+++ ewr_so_se_2024/aufgabe_2.py
@@ -9,20 +9,24 @@ def py_logspace(
     start: int, stop: int, num: int = 5, basis: int = 10, dtype: Any = int
 ) -> list[int]:
     """
-        Erstellung von Liste ganzer Zahlen mit logarithmisch konstantem Abstand
-        von M. van Straten und P. Merz
-        params: start - Legt kleinstes Element der auszugebenden Liste mit basis^start fest
-                stop  - Legt größtes Element der auszugebenden Liste mit basis^stop fest
-                num   - Anzahl der Elemente in der auszugebenden Liste
-                basis - Legt mit start und stop erstes und letztes Element fest, standardmäßig = 10
-        returns: Liste mit num Anzahl an Elementen zwischen basis^start und basis^stop
+    Erstellung von Liste ganzer Zahlen mit logarithmisch konstantem Abstand
+    von M. van Straten und P. Merz
+    params: start - Legt kleinstes Element der auszugebenden Liste mit basis^start fest
+            stop  - Legt größtes Element der auszugebenden Liste mit basis^stop fest
+            num   - Anzahl der Elemente in der auszugebenden Liste
+            basis - Legt mit start und stop erstes und letztes Element fest, standardmäßig = 10
+    returns: Liste mit num Anzahl an Elementen zwischen basis^start und basis^stop
     """
     if num < 3:
         raise ValueError
-    exp_liste = []  # Leere Liste wird erstellt, die später mit den gesuchten Elementen gefüllt wird
-    for i in range(0 ,num):     # Iteratives Erstellen neuer ELemente
-        step = (stop - start)/(num - 1)     # step ist der logarithmisch gesehene konstante Abstand
-        log_add = start + i*step            # logarithmischer konstanter Abstand wird addiert
+    exp_liste = (
+        []
+    )  # Leere Liste wird erstellt, die später mit den gesuchten Elementen gefüllt wird
+    for i in range(0, num):  # Iteratives Erstellen neuer ELemente
+        step = (stop - start) / (
+            num - 1
+        )  # step ist der logarithmisch gesehene konstante Abstand
+        log_add = start + i * step  # logarithmischer konstanter Abstand wird addiert
         exp_add = dtype(basis**log_add)  # Neues Element für die Liste wird erstellt
         exp_liste.append(exp_add)  # Das neue Element wird zur Liste hinzugefügt
     return exp_liste
