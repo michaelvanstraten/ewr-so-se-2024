b41ed453073f97cc76cf75464144ce76f3693943
Michael van Straten <michael@vanstraten.de>
2024-06-06 20:06:02 +0200

    Improve doc-strings and strings in `utils.py`

--- ewr_so_se_2024/utils.py
+++ ewr_so_se_2024/utils.py
@@ -1,13 +1,31 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-function-docstring
-# pylint: disable=missing-class-docstring
+"""
+This module contains utility functions and classes for command-line interface (CLI) options 
+and Matplotlib compatibility adjustments.
+"""
 
 import click
 
 
-# https://stackoverflow.com/a/44349292
 class NotRequiredIf(click.Option):
+    """
+    A custom Click option class that makes an option mutually exclusive with another option.
+
+    This class extends Click's Option to add the functionality where an option becomes
+    not required if another specified option is present.
+
+    Attributes:
+        not_required_if (str): The name of the option that, if present,
+                               makes this option not required.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes the NotRequiredIf option.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments. Must include 'not_required_if'.
+        """
         self.not_required_if = kwargs.pop("not_required_if")
         assert self.not_required_if, "'not_required_if' parameter required"
         kwargs["help"] = (
@@ -15,31 +33,53 @@ class NotRequiredIf(click.Option):
             + "\b\n"
             + f"NOTE: This argument is mutually exclusive with {self.not_required_if}"
         ).strip()
-        # print(kwargs["help"])
         super().__init__(*args, **kwargs)
 
     def handle_parse_result(self, ctx, opts, args):
+        """
+        Handles the parse result for the option.
+
+        This method checks if both the mutually exclusive options are present and raises a
+        UsageError if so.
+
+        Args:
+            ctx (click.Context): The Click context.
+            opts (dict): The parsed options.
+            args (list): The remaining arguments.
+
+        Returns:
+            The result of the superclass's handle_parse_result method.
+
+        Raises:
+            click.UsageError: If both mutually exclusive options are present.
+        """
         we_are_present = self.name in opts
         other_present = self.not_required_if in opts
 
         if other_present:
             if we_are_present:
                 raise click.UsageError(
-                    # pylint: disable=line-too-long
-                    f"Illegal usage: `{self.name}` is mutually exclusive with `{self.not_required_if}`"
+                    f"Illegal usage: `{self.name}` is mutually exclusive with \
+                    `{self.not_required_if}`"
                 )
             self.prompt = None
 
         return super().handle_parse_result(ctx, opts, args)
 
 
-# https://stackoverflow.com/a/75903189
 def tikzplotlib_fix_ncols(obj):
     """
-    workaround for matplotlib 3.6 renamed legend's _ncol to _ncols, which breaks tikzplotlib
+    Recursively fixes the '_ncols' attribute in Matplotlib objects for compatibility with
+    tikzplotlib.
+
+    Matplotlib 3.6 renamed the legend's '_ncol' attribute to '_ncols', which breaks tikzplotlib.
+    This function renames '_ncols' back to '_ncol' in the given object and all its children.
+
+    Args:
+        obj: A Matplotlib object, typically a figure or axis, to be fixed.
     """
     if hasattr(obj, "_ncols"):
-        # pylint: disable=protected-access)
+        # pylint: disable=protected-access
         obj._ncol = obj._ncols
     for child in obj.get_children():
         tikzplotlib_fix_ncols(child)
